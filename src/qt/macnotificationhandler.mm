4b7e4104ac0f20f234fded507390cc43980568a6
doraecoin-project <53932157+doraecoin-project@users.noreply.github.com>
2019-09-07 14:19:56 +0700

    Add files via upload

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.litecoin.Litecoin-Qt";
+        return @"org.doraecoin.Doraecoin-Qt";
     } else {
         return [self __bundleIdentifier];
     }
